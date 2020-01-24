5f1d350d26848171489f9cdb37c35299b2870fef
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-01-24 15:49:02 -0700

    Add `renderTexture` function

--- dodger/src/sdl.zig
+++ dodger/src/sdl.zig
@@ -30,3 +30,17 @@ pub fn loadTexture(ren: *SDL_Renderer, file: [*]const u8) Error!*SDL_Texture {
 
     return texture;
 }
+
+/// Renders a texture at the specified position
+pub fn renderTexture(ren: *SDL_Renderer, tex: *SDL_Texture, x: i32, y: i32) void {
+    // Setup destination rectangle to be at the right place
+    var dst: SDL_Rect = undefined;
+    dst.x = x;
+    dst.y = y;
+
+    // Query the texture's size
+    _ = SDL_QueryTexture(tex, null, null, &dst.w, &dst.h);
+
+    // Render the texture to screen at the destination rectangle
+    _ = SDL_RenderCopy(ren, tex, null, &dst);
+}
