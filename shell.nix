ca287ba2ffdff40f82920119a35177d0ae5e7e8f
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-01-24 13:50:26 -0700

    Create shell.nix with zig gamedev environment

---
+++ shell.nix
@@ -0,0 +1,30 @@
+let
+  nixpkgs = import <nixpkgs> { };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "game-clones-env";
+    buildInputs = [
+      zig
+      git
+      pkgconfig
+      openssl
+      gcc
+      libGL
+      xorg.libX11 xorg.libXcursor xorg.libXrandr xorg.libXi xorg.libxcb
+      alsaLib
+      cmake gnumake
+      freetype
+      python3
+      expat
+      ];
+    shellHook = ''
+      export RUST_BACKTRACE=1
+      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${xorg.libX11}/lib
+      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${xorg.libXcursor}/lib
+      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${xorg.libXrandr}/lib
+      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${xorg.libXi}/lib
+      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${libGL}/lib
+      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${xorg.libxcb}/lib
+    '';
+  }
