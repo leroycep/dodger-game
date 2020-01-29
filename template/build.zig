86913b6795af021fee7cd2e4bfc9c59040d23330
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-01-29 11:53:23 -0700

    Add kiwi lib and include in build.zig

--- template/build.zig
+++ template/build.zig
@@ -8,6 +8,12 @@ pub fn build(b: *Builder) void {
     exe.linkSystemLibrary("SDL2_image");
     exe.linkSystemLibrary("GLESv2");
     exe.linkSystemLibrary("c");
+    exe.addIncludeDir("lib/kiwi/src/");
+
+    const lib_cflags = [_][]const u8{};
+    inline for (KIWI_SOURCES) |src| {
+        exe.addCSourceFile("lib/kiwi/src/" ++ src, lib_cflags);
+    }
     exe.install();
 
     const run_cmd = exe.run();
@@ -16,3 +22,26 @@ pub fn build(b: *Builder) void {
     const run_step = b.step("run", "Run the game");
     run_step.dependOn(&run_cmd.step);
 }
+
+const KIWI_SOURCES = [_][]const u8{
+    "KW_scrollbox_internal.c",
+    "KW_scrollbox.c",
+    "KW_editbox_internal.c",
+    "utf8.c",
+    "KW_editbox.c",
+    "KW_eventwatcher.c",
+    "KW_button.c",
+    "KW_label.c",
+    "KW_label_internal.c",
+    "KW_gui.c",
+    "KW_frame.c",
+    "KW_frame_internal.c",
+    "KW_tilerenderer.c",
+    "KW_widget.c",
+    "KW_widget_eventhandlers.c",
+    "KW_renderdriver.c",
+    "KW_renderdriver_sdl2.c",
+    "KW_rect.c",
+    "KW_toggle.c",
+    "KW_checkbox.c",
+};
